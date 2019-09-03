a900005c8e6cb94a9a40e00bc3c3d99d8171d565
michellemoon236 <michellemoon236@gmail.com>
2019-09-02 19:13:23 -0700

    Done.

--- src/createStore.js
+++ src/createStore.js
@@ -1,6 +1,3 @@
-import candyReducer from './reducers/candyReducer';
-import countReducer from './reducers/countReducer';
-
 export default function createStore(reducer) {
   let state;
  
@@ -25,10 +22,3 @@ export default function createStore(reducer) {
 function render() {
   const container = document.getElementById('container');
 }
-
-// let candyStore = createStore(candyReducer)
-
-// let countStore = createStore(countReducer)
-
-// candyStore.
-// countStore.dispatch({ type: '@@INIT' });
